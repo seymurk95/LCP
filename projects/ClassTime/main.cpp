bb75f854d62208c3e8d667882f3b939393c51b4d
seymurk95 <seymur.kazimov.05@mail.ru>
2024-10-09 11:47:26 +0400

    changes

--- projects/ClassTime/main.cpp
+++ projects/ClassTime/main.cpp
@@ -6,14 +6,34 @@ bool IsAfternoonTime(const chron::Time& time)
 }
 int main()
 {
-	chron::Time t(1300,-30,0);
+	/*chron::Time t(1300,-30,0);
 	std::cout<<t.GetHours()<<"\n";
 	std::cout<<t.GetMinutes()<<"\n";
-	std::cout<<t.GetSeconds()<<"\n";
+	std::cout<<t.GetSeconds()<<"\n";*/
 	
 	/*if(IsAfternoonTime(t))
 	{
 		std::cout<<t.GetHours()-12<<"PM\n";
 	}*/
+	
+	chron::Time t1(10,18,-5);
+	chron::Time t2;
+	chron::Time t3(3600);
+	t2.AddSeconds(3600);
+	/*std::cout<<t1.GetHours()<<" ";
+	std::cout<<t1.GetMinutes()<<" ";
+	std::cout<<t1.GetSeconds()<<"\n";*/
+	
+	std::cout<<t2<<"\n";
+	std::cout<<t2.GetHours()<<" ";
+	std::cout<<t2.GetMinutes()<<" ";
+	std::cout<<t2.GetSeconds()<<"\n";
+	std::cout<<t2.TotalSeconds()<<"\n";
+	std::cout<<t2-t3<<"\n";
+	
+	/*std::cout<<t3.GetHours()<<" ";
+	std::cout<<t3.GetMinutes()<<" ";
+	std::cout<<t3.GetSeconds()<<"\n";*/
+	
 	
 }
--- projects/ClassTime/time.cpp
+++ projects/ClassTime/time.cpp
@@ -1,39 +1,77 @@
 #include "time.h"
 using namespace chron;
-Time::Time(int h,int m,int s)
+void Time::normalise()	
 {
-	m+=s/60;
-	s%=60;
-	if(s<0)
+	minutes+=seconds/60;
+	seconds%=60;
+	if(seconds<0)
 	{
-		m-=1;
-		s+=60;
+		minutes-=1;
+		seconds+=60;
 	}
-	h+=m/60;
-	m%=60;
-	if(m<0)
+	hours+=minutes/60;
+	minutes%=60;
+	if(minutes<0)
 	{
-		h-=1;
-		m+=60;
+		hours-=1;
+		minutes+=60;
 	}
-	h%=24;
-	if(h<0)
+	hours%=24;
+	if(hours<0)
 	{
-		h+=24;
+		hours+=24;
 	}
-	hours=h;
-	minutes=m;
-	seconds=s;
 }
+
+void Time::AddSeconds(int s)
+{
+	seconds+=s;
+	normalise();
+}
+
+Time& Time::operator +=(int s)
+{
+	seconds+=s;
+	normalise();
+	return *this;
+}
+
+Time::Time(int h,int m,int s):
+hours(h),minutes(m),seconds(s)
+{
+	normalise();
+}
+
 int Time::GetHours() const
 {
 	return hours;
 }
+
 int Time::GetMinutes() const
 {
 	return minutes;
 }
+
 int Time::GetSeconds() const
 {
 	return seconds;
+}
+
+Time Time::operator+(int s)const
+{
+	return Time(hours,minutes,seconds+s);
+}
+
+int Time::TotalSeconds()const
+{
+	return ((hours*3600)+(minutes*60)+seconds);
+}
+int operator-(const Time& t1,const Time& t2)
+{
+	return t1.TotalSeconds()-t2.TotalSeconds();
+}
+std::ostream& operator<<(std::ostream& out,const chron::Time& t)
+{
+	out<<t.GetHours()<<":"<<t.GetMinutes()<<":"<<t.GetSeconds();
+	return out;
 }
